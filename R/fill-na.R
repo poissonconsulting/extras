a5592f91dcd63c90b71544b9dc8ebe544cf5ddbc
joethorley <joe@poissonconsulting.ca>
2020-01-23 12:35:01 -0800

    fill-na

---
+++ R/fill-na.R
@@ -0,0 +1,43 @@
+#' @importFrom universals fill_na
+#' @export
+universals::fill_na
+
+#' @inherit universals::fill_na
+#' @export
+fill_na.logical <- function(x, value = FALSE, ...) {
+  chk_scalar(value)
+  chk_unused(...)
+  value <- as.logical(value)
+  x[is.na(x)] <- value
+  x
+}
+
+#' @inherit universals::fill_na
+#' @export
+fill_na.integer <- function(x, value = 0L, ...) {
+  chk_scalar(value)
+  chk_unused(...)
+  value <- as.integer(value)
+  x[is.na(x)] <- value
+  x
+}
+
+#' @inherit universals::fill_na
+#' @export
+fill_na.numeric <- function(x, value = 0, ...) {
+  chk_scalar(value)
+  chk_unused(...)
+  value <- as.numeric(value)
+  x[is.na(x)] <- value
+  x
+}
+
+#' @inherit universals::fill_na
+#' @export
+fill_na.character <- function(x, value = "0", ...) {
+  chk_scalar(value)
+  chk_unused(...)
+  value <- as.character(value)
+  x[is.na(x)] <- value
+  x
+}
---
+++ tests/testthat/test-fill-na.R
@@ -0,0 +1,21 @@
+context("fill-na")
+
+test_that("fill_na atomic objects", {
+  expect_identical(fill_na(numeric(0)), numeric(0))
+  expect_identical(fill_na(integer(0)), integer(0))
+  expect_identical(fill_na(NA), FALSE)
+  expect_identical(fill_na(c(10L, NA)), c(10L, 0L))
+  expect_identical(
+    fill_na(matrix(c(1L, 3L, 7L, NA), nrow = 2)),
+    matrix(c(1L, 3L, 7L, 0L), nrow = 2)
+  )
+  expect_identical(fill_na(c(10L, NA), value = 11L), c(10L, 11L))
+  expect_identical(fill_na(c(10L, NA), value = 11), c(10L, 11L))
+  expect_identical(
+    fill_na(matrix(c(TRUE, NA, FALSE, NA), nrow = 2),
+      value = "TRUE"
+    ),
+    matrix(c(TRUE, TRUE, FALSE, TRUE), nrow = 2)
+  )
+})
+
