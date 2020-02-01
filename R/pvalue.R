43c89b3a22645ad9d7eaf6b401aab15ceec45150
joethorley <joe@poissonconsulting.ca>
2020-02-01 13:05:23 -0800

    added pvalue function (#1)

---
+++ R/pvalue.R
@@ -0,0 +1,16 @@
+#' Bayesian P-Value
+#'
+#' @param x A numeric vector of MCMC values.
+#' @return A number between 0 and 1.
+#' @export
+#' @examples
+#' pvalue(as.numeric(0:100))
+pvalue <- function(x) {
+  chk_numeric(x)
+  if(!length(x)) return(NA_real_)
+  n <- length(x)
+  s <-  min(sum(x < 0), sum(x > 0))
+  s <- s * 2
+  s <- s + sum(x == 0)
+  (s + 1) / (n + 1)
+}
