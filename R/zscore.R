5d0d24d22d5d1b9fa54d6dce93327d1170886048
joethorley <joe@poissonconsulting.ca>
2020-02-01 18:11:56 -0800

    test zscore

--- R/zscore.R
+++ R/zscore.R
@@ -8,5 +8,8 @@
 #' zscore(as.numeric(0:100))
 zscore <- function(x) {
   chk_numeric(x)
-  mean(x)/stats::sd(x)
+  if(!length(x)) return(NA_real_)
+  x <- mean(x)/stats::sd(x)
+  if(is.nan(x)) return(NA_real_)
+  x
 }
---
+++ tests/testthat/test-zscore.R
@@ -0,0 +1,12 @@
+context("zscore")
+
+test_that("zscore", {
+  expect_identical(zscore(NA_integer_), NA_real_)
+  expect_identical(zscore(integer(0)), NA_real_)
+  expect_equal(zscore(1), NA_real_)
+  expect_equal(zscore(c(1,1)), Inf)
+  expect_equal(zscore(c(0,0)), NA_real_)
+  expect_equal(zscore(0), NA_real_)
+  set.seed(101)
+  expect_equal(zscore(rnorm(1000, 2)), 2.04891535423035)
+})
