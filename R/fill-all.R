29c78b31c2fcde2113b74c37520fb588aa1bdf28
joethorley <joe@poissonconsulting.ca>
2020-02-05 14:08:25 -0800

    fix up fill-na

--- R/fill-all.R
+++ R/fill-all.R
@@ -13,6 +13,10 @@ fill_all_impl <- function(x, value, nas) {
 #' @inherit universals::fill_all
 #' @inheritParams params
 #' @export
+#' @examples
+#' fill_all(c(TRUE, NA, FALSE))
+#' fill_all(c(TRUE, NA, FALSE, nas = FALSE))
+#' fill_all(c(TRUE, NA, FALSE, value = NA))
 fill_all.logical <- function(x, value = FALSE, nas = TRUE, ...) {
   chk_scalar(value)
   chk_flag(nas)
@@ -25,6 +29,8 @@ fill_all.logical <- function(x, value = FALSE, nas = TRUE, ...) {
 #' @inherit universals::fill_all
 #' @inheritParams params
 #' @export
+#' @examples
+#' fill_all(matrix(1:4, nrow = 2), value = -1)
 fill_all.integer <- function(x, value = 0L, nas = TRUE, ...) {
   chk_scalar(value)
   chk_flag(nas)
@@ -37,6 +43,9 @@ fill_all.integer <- function(x, value = 0L, nas = TRUE, ...) {
 #' @inherit universals::fill_all
 #' @inheritParams params
 #' @export
+#' @examples
+#' fill_all(c(1,4,NA), value = TRUE)
+#' fill_all(c(1,4,NA), value = TRUE, nas = FALSE)
 fill_all.numeric <- function(x, value = 0, nas = TRUE, ...) {
   chk_scalar(value)
   chk_flag(nas)
@@ -49,6 +58,8 @@ fill_all.numeric <- function(x, value = 0, nas = TRUE, ...) {
 #' @inherit universals::fill_all
 #' @inheritParams params
 #' @export
+#' @examples
+#' fill_all(c("some", "words"), value = TRUE)
 fill_all.character <- function(x, value = "0", nas = TRUE, ...) {
   chk_scalar(value)
   chk_flag(nas)
--- R/fill-na.R
+++ R/fill-na.R
@@ -3,6 +3,8 @@ universals::fill_na
 
 #' @inherit universals::fill_na
 #' @export
+#' @examples
+#' fill_na(c(TRUE, NA))
 fill_na.logical <- function(x, value = FALSE, ...) {
   chk_scalar(value)
   chk_unused(...)
@@ -13,6 +15,8 @@ fill_na.logical <- function(x, value = FALSE, ...) {
 
 #' @inherit universals::fill_na
 #' @export
+#' @examples
+#' fill_na(c(1L, NA), 0)
 fill_na.integer <- function(x, value = 0L, ...) {
   chk_scalar(value)
   chk_unused(...)
@@ -23,6 +27,8 @@ fill_na.integer <- function(x, value = 0L, ...) {
 
 #' @inherit universals::fill_na
 #' @export
+#' @examples
+#' fill_na(c(1, NA), Inf)
 fill_na.numeric <- function(x, value = 0, ...) {
   chk_scalar(value)
   chk_unused(...)
@@ -33,6 +39,9 @@ fill_na.numeric <- function(x, value = 0, ...) {
 
 #' @inherit universals::fill_na
 #' @export
+#' @examples
+#' fill_na(c("text", NA))
+#' fill_na(matrix(c("text", NA)), value = Inf)
 fill_na.character <- function(x, value = "0", ...) {
   chk_scalar(value)
   chk_unused(...)
