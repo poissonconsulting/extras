5038cb449fc9c741d88dea6cde7c53b2da3962ab
joethorley <joe@poissonconsulting.ca>
2020-02-05 14:15:18 -0800

    vld to chk

--- R/chk.R
+++ R/chk.R
@@ -23,3 +23,25 @@ chk_pars <- function(x, x_name = NULL) {
   chk_unique(x, x_name = x_name)
   chk_match(x, p0("^[[:alpha:]][[:alnum:]._]*$"), x_name = x_name)
 }
+
+#' Validate Parameter Names
+#'
+#' Validates a character vector of parameter names.
+#'
+#' The character vector must consist of unique, non-missing values.
+#'
+#' @inheritParams params
+#' @inheritParams chk::chk_flag
+#' @return A flag indicating whether the condition was met.
+# @seealso [chk_pars()]
+#' @export
+#' @examples
+#' vld_pars(c("x", "a1._", "X"))
+#' vld_pars(c("x[1]", "a1", "a1", "._0"))
+vld_pars <- function(x) {
+  vld_s3_class(x, "character") &&
+    vld_not_any_na(x) &&
+    vld_unique(x) &&
+    vld_match(x, p0("^[[:alpha:]][[:alnum:]._]*$"))
+}
+
