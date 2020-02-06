f5d0fb20dc17e3b24d4ee9f505e88251220c1daa
joethorley <joe@poissonconsulting.ca>
2020-02-05 18:30:41 -0800

    add numericise

---
+++ R/numericise.R
@@ -0,0 +1,109 @@
+#' @export
+universals::numericise
+
+#' @export
+universals::numericize
+
+#' @inherit universals::numericise
+#' @return A double numeric atomic object.
+#' @export
+#' @examples
+#' as.POSIXlt("1970-01-01", tz = "UTC")
+numericise.default <- function(x, ...) as.double(x)
+
+#' @inherit universals::numericise
+#' @return An integer numeric atomic object.
+#' @export
+#' @examples
+#' numericise(TRUE)
+#' numericise(matrix(c(TRUE, FALSE), nrow = 2))
+numericise.logical <- function(x, ...) as.integer(x)
+
+#' @inherit universals::numericise
+#' @return An integer numeric atomic object.
+#' @export
+#' @examples
+#' numericise(2L)
+numericise.integer <- function(x, ...) x
+
+#' @inherit universals::numericise
+#' @return A double numeric atomic object.
+#' @export
+#' @examples
+#' numericise(c(1, 3))
+numericise.double <- function(x, ...) x
+
+#' @inherit universals::numericise
+#' @return A positive integer numeric atomic vector object.
+#' @export
+#' @examples
+#' numericise(factor(c("c", "a")))
+numericise.factor <- function(x, ...) as.integer(x)
+
+#' @inherit universals::numericise
+#' @return A double numeric atomic vector object.
+#' @export
+#' @examples
+#' numericise(as.Date("1972-01-01"))
+numericise.Date <- function(x, ...) {
+  x <- unclass(x)
+  as.numeric(x)
+}
+
+#' @inherit universals::numericise
+#' @return A double numeric atomic vector object.
+#' @export
+#' @examples
+#' numericise(as.POSIXct("1972-01-01", tz = "UTC"))
+numericise.POSIXct <- function(x, ...) {
+  x <- unclass(x)
+  as.numeric(x)
+}
+
+#' @inherit universals::numericise
+#' @return A double numeric atomic matrix object.
+#' @export
+#' @examples
+#' numericise(matrix(TRUE))
+numericise.matrix <- function(x, ...) {
+  if (is.logical(x)) {
+    mode(x) <- "integer"
+    return(x)
+  }
+  dims <- dims(x)
+  x <- numericize(as.vector(x))
+  dim(x) <- dims
+  x
+}
+
+#' @inherit universals::numericise
+#' @return A double numeric atomic matrix object.
+#' @export
+#' @examples
+#' numericise(array(TRUE))
+numericise.array <- function(x, ...) {
+  if (is.logical(x)) {
+    mode(x) <- "integer"
+    return(x)
+  }
+  dims <- dims(x)
+  x <- numericize(as.vector(x))
+  dim(x) <- dims
+  x
+}
+
+#' @inherit universals::numericise
+#' @return A double numeric atomic matrix object.
+#' @export
+#' @examples
+#' numericise(data.frame(
+#'   logical = c(TRUE, FALSE, NA),
+#'   integer = 1:3,
+#'   numeric = c(4, 10, NA),
+#'   factor = as.factor(c("c", "A", "green"))
+#' ))
+numericise.data.frame <- function(x, ...) {
+  x[] <- lapply(x, numericise)
+  x <- as.matrix(x)
+  x
+}
---
+++ tests/testthat/test-numericise.R
@@ -0,0 +1,98 @@
+context("numericise")
+
+test_that("numericize", {
+  expect_identical(numericize(TRUE), 1L)
+})
+
+test_that("numericise.logical", {
+  expect_identical(numericise(logical(0)), integer(0))
+  expect_identical(numericise(TRUE), 1L)
+})
+
+test_that("numericise.integer", {
+  expect_identical(numericise(2L), 2L)
+})
+
+test_that("numericise.double", {
+  expect_identical(numericise(2), 2)
+})
+
+test_that("numericise.factor", {
+  expect_identical(numericise(factor(3:2)), c(2L, 1L))
+})
+
+test_that("numericise.character", {
+  expect_identical(numericise("1"), 1)
+})
+
+test_that("numericise.Date", {
+  expect_identical(numericise(as.Date("1970-01-02")), 1)
+  x <- 10.9
+  class(x) <- "Date"
+  expect_identical(numericise(x), 10.9)
+})
+
+test_that("numericise.POSIXct", {
+  expect_identical(numericise(as.POSIXct("1970-01-02", tz = "UTC")), 86400)
+})
+
+test_that("numericise.matrix", {
+  expect_identical(numericise(matrix(NA)), matrix(NA_integer_))
+  expect_identical(
+    numericise(matrix(NA, c(2, 3, 4))),
+    matrix(NA_integer_, c(2, 3, 4))
+  )
+  expect_identical(numericise(matrix(NA_real_)), matrix(NA_real_))
+})
+
+test_that("numericise.array", {
+  expect_identical(numericise(array(1:12, c(1, 3, 4))), array(1:12, c(1, 3, 4)))
+  expect_identical(numericise(array(TRUE, c(1, 1, 1))), array(1L, c(1, 1, 1)))
+})
+
+test_that("numericise.list", {
+  expect_identical(
+    numericise(list(x = TRUE)),
+    1
+  )
+  expect_identical(
+    numericise(list(x = TRUE, y = list(z = factor(3)))),
+    c(1, NA)
+  )
+
+  expect_error(
+    numericise(list(
+      lgl = c(TRUE, NA),
+      int = 1:2,
+      dbl = c(2.5, 1.5),
+      dte = as.Date(c("2001-01-02", "2001-01-01")),
+      fac = factor(c("b", "a"))
+    )))
+})
+
+test_that("numericise.data.frame", {
+  expect_identical(
+    numericise(data.frame(x = TRUE)),
+    structure(1L, .Dim = c(1L, 1L), .Dimnames = list(NULL, "x"))
+  )
+  expect_identical(
+    numericise(data.frame(
+      x = factor(3:2), y = c(FALSE, NA), z = c("1", "2"),
+      stringsAsFactors = FALSE
+    )),
+    structure(c(2, 1, 0, NA, 1, 2), .Dim = 2:3, .Dimnames = list(
+    NULL, c("x", "y", "z")))
+  )
+
+  expect_identical(
+    numericise(data.frame(
+      lgl = c(TRUE, NA),
+      int = 1:2,
+      dbl = c(2.5, 1.5),
+      dte = as.Date(c("2001-01-02", "2001-01-01")),
+      fac = factor(c("b", "a"))
+    )),
+    structure(c(1, NA, 1, 2, 2.5, 1.5, 11324, 11323, 2, 1), .Dim = c(2L,
+5L), .Dimnames = list(NULL, c("lgl", "int", "dbl", "dte", "fac"
+))))
+})
