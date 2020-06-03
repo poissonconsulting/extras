566316028c966a85b9708aa32e4a7467336e11ed
Joe Thorley <joe@poissonconsulting.ca>
2020-06-03 16:18:17 -0700

    define svalue (#1)

--- R/svalue.R
+++ R/svalue.R
@@ -1,8 +1,16 @@
-#' Bayesian Surprisal Value
+#' Surprisal Value
+#'
+#' The surprisal value is the probability of the [pvalue] expressed in
+#' terms of how many consecutive heads would have to be thrown on a fair coin
+#' is a single attempt to achieve the same probability.
 #'
 #'
-#'
-#' @param x A numeric vector of MCMC values.
+#' @param x A numeric object of MCMC values.
+#' @references
+#' Greenland, S. 2019. Valid P -Values Behave Exactly as They Should:
+#' Some Misleading Criticisms of P -Values and Their Resolution With S -Values.
+#' The American Statistician 73(sup1): 106–114.
+#' http://doi.org/10.1080/00031305.2018.1529625.
 #' @return A non-negative number.
 #' @family summary
 #' @export
