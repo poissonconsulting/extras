1a61f8dbe24f3b63e7dce6455baf9f4c1e8c15d8
Joe Thorley <joe@poissonconsulting.ca>
2020-07-14 13:21:36 -0700

    Added chk_index() and vld_index()

---
+++ R/chk-index.R
@@ -0,0 +1,37 @@
+#' Check Index
+#'
+#' Checks if an object is a vector of one or more positive integer values.
+#'
+#' @inheritParams params
+#' @inheritParams chk::chk_flag
+#' @return
+#' The `chk_` function throws an informative error if the test fails.
+#'
+#' The `vld_` function returns a flag indicating whether the test was met.
+#' @export
+#' @examples
+#' x <- c(2L, 1L)
+#' chk_index(x)
+#' y <- c(2L, -1L)
+#' try(chk_pars(y))
+chk_index <- function(x, x_name = NULL) {
+  if (vld_index(x)) {
+    return(invisible())
+  }
+  if (is.null(x_name)) x_name <- deparse_backtick_chk(substitute(x))
+
+  chk_integer(x, x_name = x_name)
+  chk_gt(x, x_name = x_name)
+  chk_not_any_na(x, x_name = x_name)
+  chk_not_empty(x, x_name = x_name)
+}
+
+#' @describeIn chk_index Validate Index
+#'
+#' @export
+#' @examples
+#' vld_index(c(-1))
+#' vld_pars(c(3L, 1L))
+vld_index <- function(x) {
+  vld_integer(x) & vld_gt(x) & vld_not_any_na(x) & vld_not_empty(x)
+}
---
+++ tests/testthat/test-chk-index.R
@@ -0,0 +1,15 @@
+test_that("vld_index", {
+  expect_true(vld_index(1L))
+  expect_true(vld_index(2:1))
+  expect_false(vld_index(integer(0)))
+  expect_false(vld_index(0L))
+  expect_false(vld_index(NA_integer_))
+  expect_false(vld_index(1))
+})
+
+test_that("chk_index", {
+  expect_null(chk_index(1L))
+  expect_error(chk_index(0L),
+               "^`0L` must be greater than 0, not 0[.]$",
+               class = "chk_error")
+})
