2310992a923757a238d3e74abfe76448602ac5af
Joe Thorley <joe@poissonconsulting.ca>
2021-01-07 09:40:35 -0800

    - Soft-deprecated `as_list_unnamed()` for `as_list()`.
    Closes #6

--- R/as-list.R
+++ R/as-list.R
@@ -7,6 +7,7 @@
 #' @return A list.
 #' @export
 as_list_unnamed <- function(x, ...) {
+  lifecycle::deprecate_soft("0.1.1", "as_list_unnamed()", "as_list()")
   UseMethod("as_list_unnamed")
 }
 
@@ -23,3 +24,30 @@ as_list_unnamed.default <- function(x, ...) {
     names(x) <- names
   x
 }
+
+#' As List
+#'
+#' Coerces an object to an list.
+#' All attributes are removed except any names.
+#'
+#' @inheritParams params
+#' @return A list.
+#' @export
+as_list <- function(x, ...) {
+  UseMethod("as_list")
+}
+
+#' @rdname as_list
+#' @export
+#' @examples
+#' as_list(1:3)
+#' as_list(c(x = 1, y = 2))
+as_list.default <- function(x, ...) {
+  x <- as.list(x)
+  names <- names(x)
+  attributes(x) <- NULL
+  if(!is.null(names))
+    names(x) <- names
+  x
+}
+
--- tests/testthat/test-as-list.R
+++ tests/testthat/test-as-list.R
@@ -1,5 +1,13 @@
 test_that("as_list_unnamed works", {
+  rlang::scoped_options(lifecycle_verbosity = "quiet")
   expect_identical(as_list_unnamed(1:2), list(1L, 2L))
   expect_identical(as_list_unnamed(c(x = 1L, y = 2L)), list(x = 1L, y = 2L))
   expect_identical(as_list_unnamed(integer(0)), list())
+})
+
+test_that("as_list works", {
+  rlang::scoped_options(lifecycle_verbosity = "quiet")
+  expect_identical(as_list(1:2), list(1L, 2L))
+  expect_identical(as_list(c(x = 1L, y = 2L)), list(x = 1L, y = 2L))
+  expect_identical(as_list(integer(0)), list())
 })
