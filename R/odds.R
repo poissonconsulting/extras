8321f676ed82e88a7413cef716b97ad4a04e67d1
Joe Thorley <joe@poissonconsulting.ca>
2021-06-07 09:41:22 -0700

    - Added `odd_ratio()`.

--- R/odds.R
+++ R/odds.R
@@ -5,6 +5,7 @@
 #' @param x A numeric object (vector, matrix or array) of probabilities.
 #' @return A numeric object of the the odds for each probability.
 #' @family translations
+#' @seealso [odds_ratio()]
 #' @export
 #' @examples
 #' odds(c(0, 0.5, 0.9, 1))
@@ -12,4 +13,21 @@ odds <- function(x) {
   chk_numeric(x)
   chk_range(x)
   x / (1 - x)
+}
+
+#' Odds Ratio
+#'
+#' Calculates the odds ratio for two probabilities.
+#'
+#' @param x A numeric object (vector, matrix or array) of probabilities.
+#' @param x2 A second numeric object of probabilities.
+#'
+#' @return A numeric object of the odds ratios.
+#' @seealso [odds()]
+#' @export
+#'
+#' @examples
+#' odds_ratio(0.5, 0.75)
+odds_ratio <- function(x, x2) {
+  odds(x) / odds(x2)
 }
--- tests/testthat/test-odds.R
+++ tests/testthat/test-odds.R
@@ -25,3 +25,32 @@ test_that("odds works vector", {
   expect_equal(odds(c(0, 0.1, 0.5, 0.9, NA, 1)),
                c(0, 1/9, 1, 9, NA, Inf))
 })
+
+test_that("odds_ratio errors incorrect inputs", {
+  chk::expect_chk_error(odds_ratio(NA, 0.5))
+  chk::expect_chk_error(odds_ratio("1", 0.5))
+  chk::expect_chk_error(odds_ratio(-0.1, 0.5))
+  chk::expect_chk_error(odds_ratio(1.1, 0.5))
+})
+
+test_that("odds_ratio handles edge cases", {
+  expect_identical(odds_ratio(numeric(0), numeric(0)), numeric(0))
+  expect_identical(odds_ratio(NA_real_, 0.5), NA_real_)
+  expect_identical(odds_ratio(0.5, NA_real_), NA_real_)
+  expect_equal(odds_ratio(matrix(0.9), matrix(0.9)), matrix(1))
+  expect_equal(odds_ratio(array(0.9), array(0.9)), array(1))
+})
+
+test_that("odds_ratio works scalars", {
+  expect_equal(odds_ratio(0, 0.5), 0)
+  expect_equal(odds_ratio(0.1, 0.1), 1)
+  expect_equal(odds_ratio(0.5, 0.9), 1/9)
+  expect_equal(odds_ratio(0.9, 0.5), 9)
+  expect_equal(odds_ratio(1, 0.5), Inf)
+})
+
+test_that("odds_ratio works vector", {
+  expect_equal(odds_ratio(c(0.1, 0.5, 0.9),
+               c(0.5, 0.1, NA)),
+               c(1/9,9,NA))
+})
