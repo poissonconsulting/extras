e6efcfc202eacf7fc616601c7df60ca2003731fe
Joe Thorley <joe@poissonconsulting.ca>
2022-10-26 11:30:24 -0700

    test bern and pois

--- tests/testthat/test-dev.R
+++ tests/testthat/test-dev.R
@@ -1,10 +1,10 @@
-test_that("dev_bern missing values", {
+test_that("bern missing values", {
   expect_identical(dev_bern(logical(0), integer(0)), numeric(0))
   expect_identical(dev_bern(NA, 1), NA_real_)
   expect_identical(dev_bern(1, NA), NA_real_)
 })
 
-test_that("dev_bern known values", {
+test_that("bern known values", {
   expect_identical(dev_bern(1, 1), 0)
   expect_identical(dev_bern(0, 0), 0)
   expect_identical(dev_bern(1, 0), Inf)
@@ -15,13 +15,13 @@ test_that("dev_bern known values", {
   expect_equal(dev_bern(0, 0.7), 2.40794560865187)
 })
 
-test_that("dev_bern vectorized", {
+test_that("bern vectorized", {
   expect_equal(dev_bern(0:1, 0:1), c(0,0))
   expect_equal(dev_bern(0:1, 1:0), c(Inf,Inf))
   expect_equal(dev_bern(0:1, c(0.3,0.6)), c(0.713349887877465, 1.02165124753198))
 })
 
-test_that("dev_bern vectorized missing values", {
+test_that("bern vectorized missing values", {
   expect_equal(dev_bern(c(NA,1), 0:1), c(NA,0))
   expect_equal(dev_bern(c(0,NA), 0:1), c(0,NA))
   expect_equal(dev_bern(c(0:1), c(NA,1)), c(NA,0))
@@ -232,31 +232,62 @@ test_that("deviance norm", {
   expect_equal(deviance, deviance(mod))
 })
 
-test_that("dev_pois", {
-  expect_identical(dev_pois(1,2),
-                   2 * (log_lik_pois(1, 1) - log_lik_pois(1, 2)))
-  expect_identical(dev_pois(0,2),
-                   2 * (log_lik_pois(0, 0) - log_lik_pois(0, 2)))
-  expect_equal(dev_pois(3,2),
-               2 * (log_lik_pois(3, 3) - log_lik_pois(3, 2)))
-  expect_identical(dev_pois(integer(0), integer(0)), numeric(0))
-  expect_identical(dev_pois(1, 1), 0)
-  expect_identical(dev_pois(0, 0), 0)
-  expect_identical(dev_pois(0, 1), 2)
+####
+test_that("pois missing values", {
+  expect_identical(dev_pois(logical(0), integer(0)), numeric(0))
   expect_identical(dev_pois(NA, 1), NA_real_)
   expect_identical(dev_pois(1, NA), NA_real_)
-  expect_equal(dev_pois(1, 3), dev_pois(1, 3, res = TRUE)^2)
-  expect_equal(dev_pois(c(1,3.5,4), 3, res = TRUE),
-               c(-1.34267472705186, 0.281166781094084, 0.549050616623137))
-  expect_equal(dev_pois(c(1,3,4), c(1, 3.5, 4), res = TRUE),
-               c(0, -0.274036349845144, 0))
 })
 
-test_that("dev_pois deviance", {
-  samples <- ran_pois(100)
+test_that("pois known values", {
+  expect_identical(dev_pois(1, 1), 0)
+  expect_identical(dev_pois(0, 0), 0)
+  expect_identical(dev_pois(1, 0), Inf)
+  expect_identical(dev_pois(0, 1), 2)
+  expect_identical(dev_pois(0, 2), 4)
+  expect_equal(dev_pois(1, 2), 0.613705638880109)
+  expect_identical(dev_pois(2, 2), 0)
+  expect_equal(dev_pois(3, 2), 0.432790648648986)
+  expect_equal(dev_pois(3, 2.5), 0.0939293407637276)
+})
+
+test_that("pois vectorized", {
+  expect_equal(dev_pois(0:3, 2), c(4, 0.613705638880109, 0, 0.432790648648986))
+  expect_equal(dev_pois(0:3, 0:3), rep(0, 4))
+  expect_equal(dev_pois(0:3, 3:0), c(6, 0.613705638880109, 0.772588722239781, Inf))
+})
+
+test_that("pois vectorized missing values", {
+  expect_equal(dev_pois(c(NA,1), 0:1), c(NA,0))
+  expect_equal(dev_pois(c(0,NA), 0:1), c(0,NA))
+  expect_equal(dev_pois(c(0:1), c(NA,1)), c(NA,0))
+  expect_equal(dev_pois(c(0:1), c(0,NA)), c(0,NA))
+})
+
+test_that("pois res", {
+  expect_equal(dev_pois(0, 0.5), dev_pois(0, 0.5, res = TRUE)^2)
+  expect_equal(dev_pois(0:1, c(0.3,0.6)), dev_pois(0:1, c(0.3,0.6), res = TRUE)^2)
+})
+
+test_that("pois log_lik", {
+  expect_equal(dev_pois(0:1, 0.5),
+               2 * (log_lik_pois(0:1, 0:1) - log_lik_pois(0:1, 0.5)))
+  expect_equal(dev_pois(0:1, 0.7),
+               2 * (log_lik_pois(0:1, 0:1) - log_lik_pois(0:1, 0.7)))
+})
+
+test_that("pois deviance", {
+  samples <- ran_pois(1000)
   mod <- glm(samples~1, family = poisson)
-  deviance <- sum(dev_pois(samples, exp(coef(mod)[1])))
-  expect_identical(deviance, deviance(mod))
+  deviance <- sum(dev_pois(samples, ilogit(coef(mod)[1])))
+})
+
+test_that("pois ran", {
+  set.seed(101)
+  samples <- ran_pois(1000)
+  res <- dev_pois(samples, 1, res = TRUE)
+  expect_equal(mean(res),-0.235540962010425)
+  expect_equal(sd(res),1.05758754072283)
 })
 
 test_that("dev_pois_zi", {
