2a391810c4bfb59609c1e00ef823b7e0b376d23e
Joe Thorley <joe@poissonconsulting.ca>
2022-10-26 05:28:59 -0700

    reorder log like

--- R/log-lik.R
+++ R/log-lik.R
@@ -1,82 +1,3 @@
-#' Poisson Log-Likelihood
-#'
-#' @inheritParams params
-#' @param x A non-negative whole numeric vector of values.
-#'
-#' @return An numeric vector of the corresponding log-likelihoods.
-#' @family log_lik_dist
-#' @export
-#'
-#' @examples
-#' log_lik_pois(c(1,3.5,4), 3)
-log_lik_pois <- function(x, lambda = 1) {
-  dpois(x, lambda, log = TRUE)
-}
-
-#' Zero-Inflated Poisson Log-Likelihood
-#'
-#' @inheritParams params
-#' @param x A non-negative whole numeric vector of values.
-#'
-#' @return An numeric vector of the corresponding log-likelihoods.
-#' @family log_lik_dist
-#' @export
-#'
-#' @examples
-#' log_lik_pois_zi(c(1,3.5,4), 3, prob = 0.5)
-log_lik_pois_zi <- function(x, lambda = 1, prob = 0) {
-  lpois <- dpois(x, lambda = lambda)
-  lpois <- lpois * (1 - prob)
-  zero <- x == 0
-  lpois[zero] <- lpois[zero] + prob
-  log(lpois)
-}
-
-#' Normal Log-Likelihood
-#'
-#' @inheritParams params
-#' @param x A numeric vector of values.
-#'
-#' @return An numeric vector of the corresponding log-likelihoods.
-#' @family log_lik_dist
-#' @export
-#'
-#' @examples
-#' dev_norm(c(-2:2))
-log_lik_norm <- function(x,  mean = 0, sd = 1) {
-  dnorm(x, mean = mean, sd = sd, log = TRUE)
-}
-
-#' Log-Normal Log-Likelihood
-#'
-#' @inheritParams params
-#' @param x A numeric vector of values.
-#'
-#' @return An numeric vector of the corresponding log-likelihoods.
-#' @family log_lik_dist
-#' @export
-#'
-#' @examples
-#' dev_norm(exp(c(-2:2)))
-log_lik_lnorm <- function(x,  meanlog = 0, sdlog = 1) {
-  dlnorm(x, meanlog = meanlog, sdlog = sdlog, log = TRUE)
-}
-
-#' Binomial Log-Likelihood
-#'
-#' @inheritParams params
-#' @param x A non-negative whole numeric vector of values.
-#'
-#' @return An numeric vector of the corresponding log-likelihoods.
-#' @family log_lik_dist
-#' @export
-#'
-#' @examples
-#' log_lik_binom(c(0, 1, 2), 2, 0.3)
-log_lik_binom <- function(x, size = 1, prob = 0.5) {
-  dbinom(x, size = size, prob = prob, log = TRUE)
-}
-
 #' Bernoulli Log-Likelihood
 #'
 #' @inheritParams params
@@ -92,7 +13,7 @@ log_lik_bern <- function(x, prob = 0.5) {
   log_lik_binom(x, size = 1, prob = prob)
 }
 
-#' Negative Binomial Log-Likelihood
+#' Binomial Log-Likelihood
 #'
 #' @inheritParams params
 #' @param x A non-negative whole numeric vector of values.
@@ -102,9 +23,9 @@ log_lik_bern <- function(x, prob = 0.5) {
 #' @export
 #'
 #' @examples
-#' log_lik_neg_binom(c(0, 1, 2), 2, 1)
-log_lik_neg_binom <- function(x, lambda = 1, theta = 0) {
-  dnbinom(x, mu = lambda, size = 1/theta, log = TRUE)
+#' log_lik_binom(c(0, 1, 2), 2, 0.3)
+log_lik_binom <- function(x, size = 1, prob = 0.5) {
+  dbinom(x, size = size, prob = prob, log = TRUE)
 }
 
 #' Gamma-Poisson Log-Likelihood
@@ -135,6 +56,85 @@ log_lik_gamma_pois <- function(x, lambda = 1, theta = 0) {
 #' log_lik_gamma_pois_zi(c(1,3.5,4), 3, 1, prob = 0.5)
 log_lik_gamma_pois_zi <- function(x, lambda = 1, theta = 0, prob = 0) {
   lpois <- dnbinom(x, mu = lambda, size = 1/theta)
+  lpois <- lpois * (1 - prob)
+  zero <- x == 0
+  lpois[zero] <- lpois[zero] + prob
+  log(lpois)
+}
+
+#' Log-Normal Log-Likelihood
+#'
+#' @inheritParams params
+#' @param x A numeric vector of values.
+#'
+#' @return An numeric vector of the corresponding log-likelihoods.
+#' @family log_lik_dist
+#' @export
+#'
+#' @examples
+#' dev_norm(exp(c(-2:2)))
+log_lik_lnorm <- function(x,  meanlog = 0, sdlog = 1) {
+  dlnorm(x, meanlog = meanlog, sdlog = sdlog, log = TRUE)
+}
+
+#' Negative Binomial Log-Likelihood
+#'
+#' @inheritParams params
+#' @param x A non-negative whole numeric vector of values.
+#'
+#' @return An numeric vector of the corresponding log-likelihoods.
+#' @family log_lik_dist
+#' @export
+#'
+#' @examples
+#' log_lik_neg_binom(c(0, 1, 2), 2, 1)
+log_lik_neg_binom <- function(x, lambda = 1, theta = 0) {
+  dnbinom(x, mu = lambda, size = 1/theta, log = TRUE)
+}
+
+#' Normal Log-Likelihood
+#'
+#' @inheritParams params
+#' @param x A numeric vector of values.
+#'
+#' @return An numeric vector of the corresponding log-likelihoods.
+#' @family log_lik_dist
+#' @export
+#'
+#' @examples
+#' dev_norm(c(-2:2))
+log_lik_norm <- function(x,  mean = 0, sd = 1) {
+  dnorm(x, mean = mean, sd = sd, log = TRUE)
+}
+
+#' Poisson Log-Likelihood
+#'
+#' @inheritParams params
+#' @param x A non-negative whole numeric vector of values.
+#'
+#' @return An numeric vector of the corresponding log-likelihoods.
+#' @family log_lik_dist
+#' @export
+#'
+#' @examples
+#' log_lik_pois(c(1,3.5,4), 3)
+log_lik_pois <- function(x, lambda = 1) {
+  dpois(x, lambda, log = TRUE)
+}
+
+#' Zero-Inflated Poisson Log-Likelihood
+#'
+#' @inheritParams params
+#' @param x A non-negative whole numeric vector of values.
+#'
+#' @return An numeric vector of the corresponding log-likelihoods.
+#' @family log_lik_dist
+#' @export
+#'
+#' @examples
+#' log_lik_pois_zi(c(1,3.5,4), 3, prob = 0.5)
+log_lik_pois_zi <- function(x, lambda = 1, prob = 0) {
+  lpois <- dpois(x, lambda = lambda)
   lpois <- lpois * (1 - prob)
   zero <- x == 0
   lpois[zero] <- lpois[zero] + prob
