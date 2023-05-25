0719bad65e4542719df8e4fdd7600b43a54d9e5d
Nicole Hill <nicole@poissonconsulting.ca>
2023-05-25 10:54:42 -0700

    - Added `log10()<-`, `log2()<-`, `ilog2()`, and `ilog10()` # 51

--- R/log.R
+++ R/log.R
@@ -17,6 +17,44 @@
   exp(value)
 }
 
+#' Log Base 2 Transformation
+#'
+#' Replaces a object with the base 2 exponent of value.
+#'
+#' @details A wrapper on [`exp2`]`(value)`.
+#'
+#' @param x An existing R object.
+#' @param value A numeric atomic object.
+#' @family translations
+#' @return Called for the side effect of updating `x`.
+#' @export
+#' @examples
+#' x <- NULL
+#' log2(x) <- 0.5
+#' x
+`log2<-` <- function(x, value) {
+  exp2(value)
+}
+
+#' Log Base 10 Transformation
+#'
+#' Replaces a object with the base 10 exponent of value.
+#'
+#' @details A wrapper on [`exp10`]`(value)`.
+#'
+#' @param x An existing R object.
+#' @param value A numeric atomic object.
+#' @family translations
+#' @return Called for the side effect of updating `x`.
+#' @export
+#' @examples
+#' x <- NULL
+#' log10(x) <- 0.5
+#' x
+`log10<-` <- function(x, value) {
+  exp10(value)
+}
+
 #' Inverse Log Transformation
 #'
 #' Inverse log transforms a numeric atomic object.
@@ -33,4 +71,40 @@
 
 ilog <- function(x) {
   exp(x)
+}
+
+#' Inverse Log Base 2 Transformation
+#'
+#' Inverse log transforms a numeric atomic object with base 2.
+#'
+#' @details A wrapper on [`exp`]`(value)`.
+#'
+#' @param x A numeric atomic object.
+#' @family translations
+#' @return A numeric atomic object.
+#' @export
+#' @examples
+#' x <- 1
+#' ilog2(x)
+
+ilog2 <- function(x) {
+  exp2(x)
+}
+
+#' Inverse Log Base 10 Transformation
+#'
+#' Inverse log transforms a numeric atomic object with base 10.
+#'
+#' @details A wrapper on [`exp`]`(value)`.
+#'
+#' @param x A numeric atomic object.
+#' @family translations
+#' @return A numeric atomic object.
+#' @export
+#' @examples
+#' x <- 1
+#' ilog10(x)
+
+ilog10 <- function(x) {
+  exp10(x)
 }
