733e27825bc3af74514840de9955fd962a105b13
Nicole Hill <nicole@poissonconsulting.ca>
2023-11-21 16:56:04 -0800

    Add alpha parameter

--- R/params.R
+++ R/params.R
@@ -29,5 +29,6 @@
 #' @param lower.tail A flag specifying whether to return the lower or upper tail of the distribution.
 #' @param q A vector of quantiles.
 #' @param p A vector of probabilities.
+#' @param alpha A non-negative numeric vector of the dispersion parameter for the gamma-count distribution.
 #' @name params
 NULL
