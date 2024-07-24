d82f44e0890a4b088a52e227cadb6232aed3b73a
Nicole Hill <nicole@poissonconsulting.ca>
2024-07-24 10:07:32 -0700

    Remove Rcpp changes from this pull request

--- R/dev.R
+++ R/dev.R
@@ -13,7 +13,7 @@
 #' dev_beta_binom(c(0, 1, 2), 1, 0.5, 0)
 dev_beta_binom <- function(x, size = 1, prob = 0.5, theta = 0, res = FALSE) {
   opt_beta_binom <- function(prob, x, size = size, theta = theta) {
-    -log_lik_beta_binom2(x = x, size = size, prob = prob, theta = theta)
+    -log_lik_beta_binom(x = x, size = size, prob = prob, theta = theta)
   }
   if (length(size) == 1) {
     size <- rep(size, length(x))
--- R/namespace.R
+++ R/namespace.R
@@ -1,5 +1,3 @@
 #' @import chk
 #' @importFrom stats dbinom dlnorm dnorm dpois dnbinom
-#' @useDynLib extras
-#' @importFrom Rcpp sourceCpp
 NULL
