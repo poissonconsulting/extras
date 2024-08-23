0524e1c8e8d154c2371111431a04911cbe8ea280
Nicole Hill <nicole@poissonconsulting.ca>
2024-08-23 16:11:41 -0700

    Conditially evaluate example for numericise.hms()

--- R/numericise.R
+++ R/numericise.R
@@ -77,6 +77,7 @@ numericise.POSIXct <- function(x, ...) {
 #' @examples
 #'
 #' # hms
+#' @examplesIf requireNamespace("hms")
 #' numericise(hms::as_hms("00:01:03"))
 numericise.hms <- function(x, ...) {
   # I'm not sure if this test is needed since S3 function
