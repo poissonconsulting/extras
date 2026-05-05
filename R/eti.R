680440cfcc30323fdbcdd2c586448210c083550d
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2026-05-05 14:48:09 -0700

    add `xtr_eti()`

---
+++ R/eti.R
@@ -0,0 +1,44 @@
+#' Equal-Tailed Interval
+#'
+#' Calculates Bayesian credible intervals using the equal-tailed interval (ETI),
+#' i.e., the CI such that the left and right tails outside the CI have the same
+#' coverage.
+#'
+#' @param x A numeric vector of MCMC samples.
+#' @param level A number between 0 and 1 (exclusive) specifying the probability
+#' coverage of the ETI.
+#' @param quiet A flag indicating whether to return warnings.
+#' @param na_rm A flag indicating whether to remove missing values.
+#' @return A data frame of the `lower` and `upper` limits for the credible interval.
+#' Note that the interval is not guaranteed to be one-sided or two-sided.
+#' @export
+#' @seealso [extras::xtr_ci] and [extras::xtr_hdi]
+#' @examples
+#' xtr_eti(rnorm(1e4))
+#' @name xtr_eti
+NULL
+
+xtr_eti <- function(x, level = 0.95, na_rm = FALSE, quiet = TRUE) {
+  chk_numeric(x)
+  chk_number(level)
+  chk_range(level)
+  chk_flag(na_rm)
+  chk_flag(quiet)
+
+  if(length(x) == 0) {
+    return(data.frame(lower = NA_real_, upper = NA_real_))
+  }
+
+  if (anyNA(x)) {
+    if (vld_true(na_rm)) {
+      x <- x[!is.na(x)]
+    } else {
+      return(data.frame(lower = NA_real_, upper = NA_real_))
+    }
+  }
+
+  data.frame(
+    lower = unname(quantile(x, (1 - level) / 2)),
+    upper = unname(quantile(x, (1 + level) / 2))
+  )
+}
---
+++ tests/testthat/test-eti.R
@@ -0,0 +1,12 @@
+test_that("xtr_eti() returns the correct values.", {
+  expect_equal(xtr_eti(NA_real_), data.frame(lower = NA_real_, upper = NA_real_))
+  expect_equal(xtr_eti(numeric(0)), data.frame(lower = NA_real_, upper = NA_real_))
+  expect_equal(xtr_eti(NA_integer_), data.frame(lower = NA_real_, upper = NA_real_))
+  expect_equal(xtr_eti(c(0, rep(Inf, 19))), data.frame(lower = Inf, upper = Inf))
+  expect_equal(xtr_eti(c(0, rep(-Inf, 19))), data.frame(lower = -Inf, upper = -Inf))
+  expect_equal(xtr_eti(1:100), data.frame(lower = 1 + 2.475, upper = 100 - 2.475))
+  expect_equal(xtr_eti(seq(0, 1, length.out = 1e3)),
+               data.frame(lower = 0.025, upper = 0.975))
+  expect_equal(xtr_eti(c(1:100, Inf)), data.frame(lower = 3.5, upper = 98.5))
+  expect_equal(xtr_eti(c(1:100, rep(Inf, 200))), data.frame(lower = 8.475, upper = Inf))
+})
