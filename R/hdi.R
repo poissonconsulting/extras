439f9da0c23a87cc187936efea23a99db9472523
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2026-05-05 14:14:28 -0700

    add `xtr_hdi()` and tests

---
+++ R/hdi.R
@@ -0,0 +1,63 @@
+#' Highest Density Interval
+#'
+#' Calculates credible intervals using the empirical highest density interval
+#' (HDI), i.e., the narrowest CI with the specified minimum coverage.
+#'
+#' @param x A numeric vector of MCMC samples.
+#' @param level A number between 0 and 1 (exclusive) specifying the probability
+#' coverage of the HDI.
+#' @param quiet A flag indicating whether to return warnings.
+#' @param na_rm A flag indicating whether to remove missing values.
+#' @return A data frame of the `lower` and `upper` limits for the credible interval.
+#' Note that the interval is not guaranteed to be one-sided or two-sided.
+#' @export
+#' @seealso [mcmcr::coef]
+#' @examples
+#' xtr_hdi(rnorm(1e4))
+#' @name xtr_hdi
+NULL
+
+xtr_hdi <- function(x, level = 0.95, na_rm = FALSE, quiet = TRUE) {
+  chk_numeric(x)
+  chk_number(level)
+  chk_range(level)
+  chk_flag(na_rm)
+  chk_flag(quiet)
+
+  if (anyNA(x)) {
+    if (vld_true(na_rm)) {
+      x <- x[!is.na(x)]
+    } else {
+      return(data.frame(lower = NA_real_, upper = NA_real_))
+    }
+  }
+  x <- sort(x)
+  n <- length(x)
+
+  if (n < 1 / (1 - level)) {
+    return(data.frame(lower = NA_real_, upper = NA_real_))
+  }
+
+  n_in <- ceiling(n * level)
+  n_out <- n - n_in
+
+  if (sum(is.infinite(x)) >= n_in) {
+    return(data.frame(lower = Inf * sign(min(x[is.infinite(x)])),
+                      upper = Inf * sign(max(x[is.infinite(x)]))))
+  }
+
+  widths <- sapply(1:n_out, function(.i) x[.i + n_in] - x[.i])
+  narrowest_is <- which(widths == min(widths)) # which.min() returns first min
+
+  if (length(narrowest_is) == 1) {
+    narrowest_i <- narrowest_is
+  } else {
+    # not testing for non-consecutive intervals: seems inconsequential
+    narrowest_i <- round(xtr_median(narrowest_is))
+  }
+
+  data.frame(
+    lower = x[narrowest_i],
+    upper = x[narrowest_i + n_in]
+  )
+}
---
+++ tests/testthat/test-hdi.R
@@ -0,0 +1,12 @@
+test_that("xtr_hdi() returns the correct values.", {
+  expect_equal(xtr_hdi(NA_real_), data.frame(lower = NA_real_, upper = NA_real_))
+  expect_equal(xtr_hdi(numeric(0)), data.frame(lower = NA_real_, upper = NA_real_))
+  expect_equal(xtr_hdi(NA_integer_), data.frame(lower = NA_real_, upper = NA_real_))
+  expect_equal(xtr_hdi(c(0, rep(Inf, 19))), data.frame(lower = Inf, upper = Inf))
+  expect_equal(xtr_hdi(c(0, rep(-Inf, 19))), data.frame(lower = -Inf, upper = -Inf))
+  expect_equal(xtr_hdi(1:100), data.frame(lower = 3, upper = 98))
+  expect_equal(xtr_hdi(seq(0, 1, length.out = 1e3)),
+               data.frame(lower = 0.032032032032032, upper = 0.982982982982983))
+  expect_equal(xtr_hdi(c(1:100, Inf)), data.frame(lower = 2, upper = 98))
+  expect_equal(xtr_hdi(c(1:100, rep(Inf, 200))), data.frame(lower = 8, upper = Inf))
+})
