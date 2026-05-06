e47271465fbb485dc05a5893e8c128a7efb2c047
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2026-05-06 10:44:28 -0700

    fix tests

--- tests/testthat/test-ci-hdi.R
+++ tests/testthat/test-ci-hdi.R
@@ -2,8 +2,8 @@ test_that("xtr_ci_hdi() returns the correct values.", {
   expect_equal(xtr_ci_hdi(NA_real_), tibble::tibble(lower = NA_real_, upper = NA_real_))
   expect_equal(xtr_ci_hdi(numeric(0)), tibble::tibble(lower = NA_real_, upper = NA_real_))
   expect_equal(xtr_ci_hdi(NA_integer_), tibble::tibble(lower = NA_real_, upper = NA_real_))
-  expect_equal(xtr_ci_hdi(c(0, rep(Inf, 19))), tibble::tibble(lower = Inf, upper = Inf))
-  expect_equal(xtr_ci_hdi(c(0, rep(-Inf, 19))), tibble::tibble(lower = -Inf, upper = -Inf))
+  expect_equal(xtr_ci_hdi(c(0, rep(Inf, 19))), tibble::tibble(lower = 0, upper = Inf))
+  expect_equal(xtr_ci_hdi(c(0, rep(-Inf, 19))), tibble::tibble(lower = -Inf, upper = 0))
   expect_equal(xtr_ci_hdi(1:100), tibble::tibble(lower = 3, upper = 98))
   expect_equal(xtr_ci_hdi(seq(0, 1, length.out = 1e3)),
                tibble::tibble(lower = 0.032032032032032, upper = 0.982982982982983))
--- tests/testthat/test-ci.R
+++ tests/testthat/test-ci.R
@@ -2,5 +2,4 @@ test_that("xtr_ci() works with all CI types.", {
   expect_no_error(xtr_ci(1:100))
   expect_no_error(xtr_ci(1:100, type = "HDI"))
   expect_no_error(xtr_ci(1:100, type = "ETI"))
-  expect_no_error(xtr_ci(1:100, type = "normal"))
 })
