4dbac14297a8ca566d1673ec9cbf66dbb6882d48
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2026-05-06 10:56:15 -0700

    clean up `rope()` args and doc

--- R/rope.R
+++ R/rope.R
@@ -1,17 +1,17 @@
 #' Region of Practical Equivalence
 #'
 #' Calculates the proportion of the posterior (or credible interval if
-#' `ci_level < 1`) that falls within the region of practical equivalence (ROPE)
+#' `level < 1`) that falls within the region of practical equivalence (ROPE)
 #' to a `threshold`.
 #' The ROPE is calculated as `threshold + interval`.
 #' Note that the default is not appropriate for all models, since the `interval`
 #' is sensitive to unit conversions.
 #'
 #' @param x A numeric vector of MCMC samples.
-#' @param ci_level A number > 0 and <= 1 specifying the probability coverage of
+#' @param level A number > 0 and <= 1 specifying the probability coverage of
 #' the HDI to use.
 #' The default of 1 uses the full posterior.
-#' @param quiet A flag indicating whether to return warnings.
+#' @param ... Currently unused.
 #' @param na_rm A flag indicating whether to remove missing values.
 #' @return A number indicating the estimated proportion of the posterior (or
 #' credible interval) within the region of practical equivalence.
@@ -23,15 +23,15 @@
 #' @name rope
 NULL
 
-rope <- function(x, threshold = 0, interval = c(-0.1, 0.1), ci_level = 1,
-                 type = "HDI", na_rm = FALSE, quiet = TRUE) {
+rope <- function(x, threshold = 0, interval = c(-0.1, 0.1), ..., level = 1,
+                 type = "HDI", na_rm = FALSE) {
   chk_numeric(x)
+  chk_number(threshold)
   chk_numeric(interval)
   chk_length(interval, 2)
-  chk_number(ci_level)
-  chk_true(all(ci_level > 0 & ci_level <= 1))
+  chk_number(level)
+  chk_range(level)
   chk_flag(na_rm)
-  chk_flag(quiet)
 
   if (anyNA(x)) {
     if (vld_true(na_rm)) {
@@ -41,8 +41,8 @@ rope <- function(x, threshold = 0, interval = c(-0.1, 0.1), ci_level = 1,
     }
   }
 
-  if(ci_level < 1) {
-    ci <- xtr_ci(x = x, level = ci_level, type = type, na_rm = na_rm, quiet = quiet)
+  if(level < 1) {
+    ci <- xtr_ci(x = x, level = level, ..., type = type, na_rm = na_rm)
     x <- x[x >= ci[1] & x <= ci[2]]
   }
   mean(x >= threshold + interval[1] & x <= threshold + interval[2])
