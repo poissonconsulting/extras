95cb5988585911afdadef1c506af3953a538dbd6
Stefano Mezzini <stefano@poissonconsulting.ca>
2026-05-22 06:08:01 -0700

    order by appearance in arguments
    Co-authored-by: Joe Thorley <joe@poissonconsulting.ca>

--- R/ci_hdi.R
+++ R/ci_hdi.R
@@ -24,8 +24,8 @@ xtr_ci_hdi <- function(x, level = 0.95, ..., na_rm = FALSE) {
   chk_number(level)
   chk_range(level, inclusive = TRUE)
   chk_gt(level)
-  chk_flag(na_rm)
   chk_unused(...)
+  chk_flag(na_rm)
 
   if(is.integer(x)) {
     na <- NA_integer_
