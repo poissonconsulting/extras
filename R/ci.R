80ba9ac9ab43de739e549cd7f05854427ec7ce9d
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2026-06-01 13:28:43 -0700

    clean up checks

--- R/ci.R
+++ R/ci.R
@@ -18,9 +18,10 @@
 #' @examples
 #' xtr_ci(rnorm(1e4), type = "HDI")
 xtr_ci <- function(x, level = 0.95, ..., type = "HDI", na_rm = FALSE) {
+  chk_unused(...)
   chk_string(type)
   chk_subset(type, c("HDI", "ETI"))
-  chk_unused(...)
+  chk_flag(na_rm)
 
   if (type == "HDI") {
     return(xtr_ci_hdi(x = x, level = level, na_rm = na_rm))
